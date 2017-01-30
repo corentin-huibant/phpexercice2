fa3e78291b327b5571b99013d131126e81bb5858
Corentin <corentin.huibant@laposte.net>
2017-01-30 12:22:46 +0100

    le deuxième exercice : suppression d'une lettre dans une chaine

---
+++ exercice2.php
@@ -0,0 +1,6 @@
+<?php
+$variable = str_replace("e", "", "La Vie aquatique (The Life Aquatic with Steve Zissou), film américain sorti en 2004, est le quatrième long-métrage réalisé par Wes Anderson.
+Il est dédié à l'océanographe français Jacques-Yves Cousteau et s'inspire librement de sa vie sur un ton parodique. Le film est une comédie dramatique qui,
+sous un aspect fantaisiste, aborde des thèmes forts comme la famille, le deuil, la vengeance, l'angoisse de vieillir ou l'échec.");
+print $variable;
+?>
